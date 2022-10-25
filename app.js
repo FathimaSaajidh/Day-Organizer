7ef36bb9a1d2eca1309097069e288cad5539ff96
creativecoding <webdev.inventions@gmail.com>
2022-10-25 08:49:07 +0530

    final commit for now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const app = express();
 const $ = require("jquery");
+const content= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Eget velit aliquet sagittis id consectetur purus. Leo in vitae turpis massa sed elementum tempus egestas. In metus vulputate eu scelerisque felis imperdiet. Dis parturient montes nascetur ridiculus mus mauris vitae. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Quis blandit turpis cursus in hac habitasse platea dictumst quisque. Nunc congue nisi vitae suscipit tellus mauris a diam. Sit amet justo donec enim."
 
 app.set('view engine', 'ejs');
 
@@ -27,12 +28,35 @@ const itemSchema = new mongoose.Schema({
     default: Date.now()
   }
 });
-// itemSchema.path('date').index({expires:});
 const ToDoItem = mongoose.model("ToDoItem", itemSchema);
-
-const item1 = new ToDoItem({activity:"Plan your day"});
-const item2 = new ToDoItem({activity:"note down your main focus for today"});
+const item1 = new ToDoItem({
+  activity: "Hey there, Welcome!"
+});
+const item2 = new ToDoItem({
+  activity: "What is your main focus today?"
+});
 const defaultItems = [item1,item2];
+
+const customlstSchema = new mongoose.Schema({
+  title : String,
+  item:[itemSchema],
+  date:{
+    type: Date,
+    default: Date.now()
+  }
+
+});
+// itemSchema.path('date').index({expires:});
+const CustomList = mongoose.model("CustomList",customlstSchema);
+
+// const helpContSchema = new mongoose.Schema({
+//   content:String
+// });
+//
+// const Help = mongoose.model("Help", helpContSchema);
+//
+// const help = new Help({
+  // });
 
 const date = new Date();
 let options = {
@@ -41,46 +65,98 @@ let options = {
   month: "long"
 };
 const today = date.toLocaleDateString("en-US", options);
+
 app.get("/", function(req,res){
   ToDoItem.find({}, function(err, foundItems){
     if(!err){
-      res.render("index",{
-        currentDay: today,
-        presentItems:foundItems
-      });
+      if(foundItems.length===0){
+        ToDoItem.insertMany(defaultItems, function(err){
+          if(!err){
+            console.log("successfully inserted default documents")
+          }
+        });
+        res.redirect("/");
+      }else{
+        res.render("index",{
+          listTitle:"ToDo",
+          currentDay: today,
+          presentItems:foundItems
+        });
+      }
     }
   });
 });
+
+app.get("/category/:customListname", function(req,res){
+  const customListname = _.capitalize(req.params.customListname);
+  CustomList.findOne({name: customListname}, function(err,result){
+    if(!err){
+      if(!result){
+        const defaultcustList = new CustomList({
+          title: customListname,
+          item: defaultItems
+        });
+        defaultcustList.save();
+        res.redirect("/category/"+customListname);
+      }else{
+        res.render("index",{
+          listTitle:result.title,
+          currentDay: today,
+          presentItems:result.item
+        });
+      }
+    }
+  });
+});
+
+app.get("/help", function(req, res) {
+
+  res.render("help",{helpContent:content});
+});
+
+
 app.post("/", function(req, res) {
   const newActivity = req.body.newItem;
-
+  const listName = req.body.list;
   const nextActivity = new ToDoItem({
     activity: newActivity
   });
+
+if(listName === "ToDo"){
   nextActivity.save();
   console.log("item saved to db");
   res.redirect("/");
+}else{
+  CustomList.findOne({title:listName}, function(err, foundList){
+    foundList.item.push(nextActivity);
+    foundList.save();
+    res.redirect("/category/"+listName);
+  });
+}
+
+
+
+
 });
-
-app.get("/lists", function(req, res) {
-
-
-  res.render("lists");
-});
-
-
-
 
 app.post("/delete", function(req, res) {
   const idofChecked = req.body.check;
-  ToDoItem.findByIdAndRemove(idofChecked, function(err) {
-    if (!err) {
-      console.log("successfully deleted an item");
-    } else {
-      console.log("id not matching");
-    }
-    res.redirect("/");
-  });
+  const listName = req.body.custlistName;
+  if(listName==="Today"){
+    ToDoItem.findByIdAndRemove(idofChecked, function(err) {
+      if (!err) {
+        console.log("successfully deleted checked item");
+        res.redirect("/");
+      }
+    });
+  }else{
+    CustomList.findOneAndUpdate({title:listName},{$pull:{item:{_id:idofChecked}}}, function(err, foundList){
+      if(!err){
+        res.redirect("/category/"+listName);
+      }
+    });
+  }
+
 
 });
 
